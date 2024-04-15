982faaef34e9867a92bc80c4173461e890b1424f
Jun Wang <junwang2003@gmail.com>
2024-04-14 21:59:34 -0500

    update config files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://edenprairie.github.io/docs/',
+  url: 'https://docs.junwang.us',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
